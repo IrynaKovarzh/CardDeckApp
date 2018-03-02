1907c4808b9d78da9f4f3b8a36c1efc449f5b27f
IrynaKovarzh <iryna.kovarzh@gmail.com>
2018-03-02 17:16:39 +0100

    Take a card: to move  and scale

--- wwwroot/UI/Script/takecard.js
+++ wwwroot/UI/Script/takecard.js
@@ -5,8 +5,18 @@ function takecard(form) {
       form.getElementsByClassName('front')[0].style.transform = 'rotate3d(0,1,0, 0deg)';
         state = 0;
     } else {
+        //the top card
+        //to move
+      form.getElementsByClassName('card')[0].style.transform = 'translate(400px,-400px)';
+
+      //to scale
+       form.getElementsByClassName('card')[0].style.transform = 'scale(0.4,0.4)';
+
+      /* 
+      //to flip the next one, in case it is exist  
       form.getElementsByClassName('front')[0].style.transform = 'rotate3d(0,1,0, 180deg)';
       form.getElementsByClassName('back')[0].style.transform = 'rotate3d(0,1,0, 0deg)';   
+     */
         state = 1;
     }        
 }
