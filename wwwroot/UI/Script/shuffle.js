602b7124c937b3b89589bc46419b6a01bec0de36
IrynaKovarzh <iryna.kovarzh@gmail.com>
2018-03-09 15:31:49 +0100

    SHUFFLE procedure

--- wwwroot/UI/Script/shuffle.js
+++ wwwroot/UI/Script/shuffle.js
@@ -5,21 +5,44 @@ function shuffle(form) {
     }
 
     var cards = form.querySelectorAll(".card.not-flipped");
-
     if (cards.length > 0) {
 
-        for (var i = 0; i < cards.length; i++) {
-            card = cards[i];
-            var toshift = 20 + i * 3;
+        for (var iiii = 0; iiii < cards.length; iiii++) {
+            card = cards[iiii];
+            var toshift = 20 + iiii * 3;
             card.style.transform = 'translate3d(-' + toshift + '%, -' + toshift + '%, 0) scale(0.5)';
         }
 
         var myVar2 = setTimeout(function () {
-            for (var i = 0; i < cards.length; i++) {
-                card = cards[i];
-                var toshift = i * 3;
+            for (var iiiii = 0; iiiii < cards.length; iiiii++) {
+                card = cards[iiiii];
+                var toshift = iiiii * 3;
                 card.style.transform = 'translate3d(-' + toshift + '%, -' + toshift + '%, 0) scale(1)';
             }
         }, 800);
+    }
+
+    var deckContainerElement = form.querySelector(".board .deck-container");
+
+    var childrenList = deckContainerElement.children;
+    var cardElements = [];
+    for (var k = 0; k < childrenList.length; k++) {
+        var element = childrenList[k].querySelectorAll(".card.not-flipped");
+        if (element && element.length != 0) {
+            cardElements.push(childrenList[k]);
+        }
+    }
+
+    //Fisher yates shuffle
+    var i = cardElements.length;
+    if (i == 0) return;
+    var ddd = document.createElement("absolute-center");
+    while (--i) {
+        var j = Math.floor(Math.random() * (i + 1));
+        if (i != j) {
+            deckContainerElement.replaceChild(ddd, cardElements[i]);
+            deckContainerElement.replaceChild(cardElements[i], cardElements[j]);
+            deckContainerElement.replaceChild(cardElements[j], ddd);
+        }
     }
 }
--- wwwroot/UI/Script/takecard.js
+++ wwwroot/UI/Script/takecard.js
@@ -1,22 +1,22 @@
 var lastCard;
 
 function takecard(form) {
-    if(lastCard) {
+    if (lastCard) {
         lastCard.style.transform = 'translate3d(200%, -25%, 0) scale(0.3)';
     }
 
     var cards = form.querySelectorAll(".card.not-flipped");
 
-    if(cards.length > 0) {
-        lastCard = cards[cards.length-1];
+    if (cards.length > 0) {
+        lastCard = cards[cards.length - 1];
 
-        lastCard.getElementsByClassName('back')[0].style.transform = 'rotate3d(0,1,0, 180deg)';   
+        lastCard.getElementsByClassName('back')[0].style.transform = 'rotate3d(0,1,0, 180deg)';
         lastCard.getElementsByClassName('front')[0].style.transform = 'rotate3d(0,1,0, 0deg)';
         lastCard.classList.add("flipped");
         lastCard.classList.remove("not-flipped");
 
         //To shift a bit a top flipped card
-        lastCard.style.transform = 'translate3d(-5%, -5%, 0)' ;
+        lastCard.style.transform = 'translate3d(-5%, -5%, 0)';
     }
-           
+
 }
