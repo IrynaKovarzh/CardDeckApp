f850c6a70de924633627b8076bde9f73deaca186
IrynaKovarzh <iryna.kovarzh@gmail.com>
2018-03-12 15:26:39 +0100

    To vizualize a deck on the start of the game

--- wwwroot/UI/Script/card.js
+++ wwwroot/UI/Script/card.js
@@ -1,8 +1,11 @@
 function Card(cardIndex) {
-    var toshift = cardIndex * 5;
+    
+    var toshift = cardIndex * 3;
+    var tr = "translate3d(-" + toshift + "%, -" + toshift + "%, 0px);";
+
     return (`
         <div class="absolute-center">
-            <div class="card not-flipped backface">
+            <div class="card not-flipped backface" style="transform: ${tr}">
                 <div class="back" style="background-image: url('${ data['back-image']}');"></div>
                 <div class="front" style="background-image: url('${ data.cards[cardIndex]['front-image']}')"></div>
             </div>
