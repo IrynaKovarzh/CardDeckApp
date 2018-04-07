7989473876f4489a0836f3fb015e81b87196024b
IrynaKovarzh <iryna.kovarzh@gmail.com>
2018-04-07 21:32:35 +0200

    Added shaffling

--- carddeckapp/src/Deck.js
+++ carddeckapp/src/Deck.js
@@ -8,41 +8,53 @@ class Deck extends Component {
 
     render() {
         const deckdata = this.props.data;
-        const cards = deckdata['cards'];
 
-        //to shaffle
-        /*  const cardsAmount = cards.length;
-  
-          if (!cardsAmount) { return; } //to set default? leave an empty? or show a message?
-  
-          //Fisher yates shuffle
-          var i = cardsAmount;
-          //if (i == 0) return;
-          while (--i) {
-              var j = Math.floor(Math.random() * (i + 1));
-              if (i !== j) {
-                  var tempi = cards[i];
-                  var tempj = cards[j];
-                  cards[j] = tempi;
-                  cards[i] = tempj;
-              }
-          } 
-          deckdata['cards'] = cards; */
+        let deck =
+            (<div>
+                <p>It is an empty deck.</p>
+            </div>);
 
-        let k = 0;
+        if (deckdata) {  //has a value
+            const cards = deckdata['cards'];
+
+            if (cards) {
+                const cardsAmount = cards.length;
+                if (cardsAmount) { //to set default? leave an empty? or show a message?
+
+                    //to shuffle 
+                    //Fisher yates shuffle
+                    var i = cardsAmount;
+                    while (--i) {
+                        var j = Math.floor(Math.random() * (i + 1));
+                        if (i !== j) {
+                            var tempi = cards[i];
+                            var tempj = cards[j];
+                            cards[j] = tempi;
+                            cards[i] = tempj;
+                        }
+                    }
+                    deckdata['cards'] = cards;
+
+                    let k = 0;
+                    deck = (<div className="deck-container">
+                        {
+                            cards.map((card, index) => (
+                                <Card
+                                    cardIndex={index}
+                                    backImage={deckdata['back-image']}
+                                    frontImage={card['front-image']}
+                                    key={k++}
+                                />
+                            ))
+                        }
+                    </div>);
+                }
+            }
+        }
 
         return (
-            <div className="deck-container">
-                {
-                    cards.map((card, index) => (
-                        <Card
-                            cardIndex={index}
-                            backImage={deckdata['back-image']}
-                            frontImage={card['front-image']}
-                            key={k++}
-                        />
-                    ))
-                }
+            <div>
+                {deck}
             </div>
         );
     }
