14298ea85e8775c8c511846e40bb9bf713f42300
IrynaKovarzh <iryna.kovarzh@gmail.com>
2018-04-08 05:32:49 +0200

    Shuffling All on the React part.

--- carddeckapp/src/DeckContainer.js
+++ carddeckapp/src/DeckContainer.js
@@ -21,7 +21,7 @@ class DeckContainer extends Component {
         this.props.history.push('/mydecks');
     }
 
-    toShuffleHandler() {
+    shuffle() {
         var deckContainerElement = document.querySelector(".board .deck-container");
 
         var childrenList = deckContainerElement.children;
@@ -69,6 +69,42 @@ class DeckContainer extends Component {
                 }
             }, 800);
         }
+    }
+
+    toShuffleHandler() {
+        this.shuffle();
+    }
+
+    toShuffleAllHandler() {
+        //To flip back all flipped cards
+        var flippedCards = document.querySelectorAll(".card.flipped");
+        if (flippedCards.length > 0) {
+            let card;
+            for (var i = 0; i < flippedCards.length; i++) {
+                card = flippedCards[i];
+
+                card.getElementsByClassName('back')[0].style.transform = 'rotate3d(0,1,0, 0deg)';
+                card.getElementsByClassName('front')[0].style.transform = 'rotate3d(0,1,0, 180deg)';
+
+                card.classList.add("not-flipped");
+                card.classList.remove("flipped");
+            }
+        }
+
+        //To move all in one deck
+        var cards = document.querySelectorAll(".card.not-flipped");
+        if (cards.length > 0) {
+            var myVar = setTimeout(function () {
+                let card;
+                for (var i = 0; i < cards.length; i++) {
+                    card = cards[i];
+                    card.style.transform = 'translate3d( 0%, 0%, 0) scale(1)';
+                }
+            }, 800);
+        }
+
+        //to shuffle
+        this.shuffle();
     }
 
     takeCardHandler() {
@@ -133,6 +169,7 @@ class DeckContainer extends Component {
                 <div>
                     <Button clicked={this.takeCardHandler.bind(this)}>TAKE A CARD</Button>
                     <Button clicked={this.toShuffleHandler.bind(this)}>SHUFFLE</Button>
+                    <Button clicked={this.toShuffleAllHandler.bind(this)}>SHUFFLE ALL</Button>
                     <Button clicked={this.closeHandler.bind(this)}>Closed</Button>
                 </div>
                 <div>
